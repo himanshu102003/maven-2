5ca39d0ddf47d4f18196a8ea58a6d5d839e40325
Himanshu Gupta <109969182+himanshu102003@users.noreply.github.com>
2025-01-02 20:08:02 +0530

    Update App.java

--- src/main/java/com/example/automation/App.java
+++ src/main/java/com/example/automation/App.java
@@ -3,13 +3,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
     public static void main(String[] args) {
         // Just a placeholder to show this is a main app entry point.
        private static final Logger logger = LoggerFactory.getLogger(App.class);
-
-    public static void main(String[] args) {
-        // Logging with an info level message
         logger.info("Application is starting...");
 
-    }
 }
